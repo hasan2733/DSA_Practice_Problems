e7af71d8f8b45ebd0e228af69a06c3f2b97a8bc4
Abid Hasan <hasan.tng33@gmail.com>
2025-05-03 22:25:23 +0600

    Create reverse_the_array.cpp

---
+++ Arrays/reverse_the_array.cpp
@@ -0,0 +1,50 @@
+/{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+
+class Solution {
+  public:
+    void reverseArray(vector<int> &arr) {
+        vector<int>temp;
+        for(int i=arr.size()-1;i>=0;i--)
+        {
+            temp.push_back(arr[i]);
+        }
+        for(int i=0;i<arr.size();i++)
+        {
+            arr[i] = temp[i];
+        }
+        
+    }
+};
+
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    cin.ignore();
+    while (t--) {
+        vector<int> arr;
+        string input;
+        getline(cin, input);
+        stringstream ss(input);
+        int number;
+        while (ss >> number) {
+            arr.push_back(number);
+        }
+
+        Solution ob;
+        ob.reverseArray(arr);
+        for (int i = 0; i < arr.size(); i++) {
+            cout << arr[i] << " ";
+        }
+        cout << endl;
+        cout << "~" << endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
